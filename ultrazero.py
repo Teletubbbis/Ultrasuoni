2184a6b8c65eeee481bdc23cae484374947335a8
Teletubbbis <108279542+Teletubbbis@users.noreply.github.com>
2024-11-14 11:57:06 +0100

    Create ultrazero.py

---
+++ ultrazero.py
@@ -0,0 +1,8 @@
+from gpiozero import DistanceSensor
+from time import sleep
+
+sensor = DistanceSensor(echo=24, trigger=23)
+
+print("Rilevazione oggetto.....")
+sensor.wait_for_in_range()
+print("Oggetto rilevato nel range d'azione!!!!")
